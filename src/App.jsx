7023eca45ee87a049e6e83146d3239f703ad0804
juliaalmeidaa4 <rm95308@estudante.fieb.edu.br>
2025-04-11 12:05:55 +0000

    vercel alterações

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,48 @@
 import { useState } from "react";
+import poke from './assets/pokeapi_256.png'
 
 function App() {
 
-  const [nome, setNome] = useState("Memphis")
+  const Formulario = (props) => {
 
-  // var nome = "Julia";
+    const [nome, setNome] = useState("Memphis")
+
+    return (
+      <div>
+
+        <input
+          className="nome"
+          name = "nome"
+          onChange={(e)=>{setNome(e.target.value)}}
+          placeholder={props.sombra != null ? props.sombra : "Texto padrão"}
+          type="text" />
+
+        <button 
+          className="botao"
+          onClick={()=>{alert(nome)}} >
+            CLIQUE AQUI
+        </button>
+
+      </div>
+    )
+  }
 
   return (
     <div>
       <h3>Pizzaria 2F</h3>
-      <input 
-      className="nome"
-      name = "nome"
-      onChange={(e)=>{nome=e.target.value}}
-      placeholder="Digite um nome..."
-      type="text" />
-      <button 
-      className="botao"
-      onClick={()=>{alert(nome)}}>
-        CLIQUE AQUI
-      </button>
+      <p>Texto do parágrafo</p>
+      <img src={poke} style={{width:200, height:100}} />
+      <img src={poke} style={{width:200, height:100}} />
+      <Formulario sombra="Digite seu nome..." />
+      <Formulario sombra="Digite seu e-mail..."/>
+      <Formulario sombra="(99)90000-0000"/>
+      <Formulario />
+      <Formulario />
+      <Formulario />
+
     </div>
   )
 }
 
 export default App
+ 
